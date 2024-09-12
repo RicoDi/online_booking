6148798564c25e5c368956abbd69d37f53446025
RicoDi <d.mamalkov@gmail.com>
2024-09-12 19:30:58 +0300

    new file:   server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Указываем, что статические файлы будут находиться в папке "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Маршрут для главной страницы
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
