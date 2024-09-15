1d39dde0613ab54af89bb74119cb8ced8edf75ba
RicoDi <d.mamalkov@gmail.com>
2024-09-15 12:32:59 +0300

    modified:   apiRouter.js 	modified:   public/css/style.css 	modified:   public/html/index.html 	modified:   public/js/calendar.js 	modified:   public/js/dist/calendar.dev.js 	modified:   public/js/script.js 	dist/apiRouter.dev.js 	public/js/dist/script.dev.js

---
+++ dist/server.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var express = require("express");
+
+var path = require("path");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000; // Импортируем наш роутер
+
+var apiRouter = require("./apiRouter"); // Указываем папку для статических файлов (CSS, JS, изображения и т.д.)
+
+
+app.use(express["static"](path.join(__dirname, "public"))); // Подключаем body-parser для обработки данных из форм
+
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // Подключаем роутер для API
+
+app.use("/api", apiRouter); // Маршрут для главной страницы
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "html", "index.html"));
+}); // Запуск сервера
+
+app.listen(PORT, function () {
+  console.log("\u0421\u0435\u0440\u0432\u0435\u0440 \u0437\u0430\u043F\u0443\u0449\u0435\u043D \u043D\u0430 http://localhost:".concat(PORT));
+});
